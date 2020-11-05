c1a6b9f32cf7278178c9b9e90e1f7efb0de90b0e
Dijinx <dimajinx@gmail.com>
2020-11-05 13:19:21 +0700

    переписал код для високосного года

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -20,7 +20,7 @@ public class FirstClass {
 		positiveOrNegative(2);
 		System.out.println(positiveOrNegative2(1));
 		hiName();
-		leapYear(2400);
+		leapYear(2700);
 	}
 
 	//Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
@@ -68,10 +68,10 @@ public class FirstClass {
 	//кроме каждого 100-го, при этом каждый 400-й – високосный.
 
 	public static void leapYear(int a) {
-		if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0)
-			System.out.println("Год номер " + a + " является високосным!");
-		else System.out.println("Год номер " + a + " не является високосным!");
-
+		if (a % 4 == 0 || a%400==0)
+			System.out.println("високосный");
+		if (a % 100 == 0 && a % 4 == 0)
+			System.out.println("не високосный");
 
 	}
 }
